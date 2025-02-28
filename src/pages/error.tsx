41995ec1ab272bba08551db2b3f461e3069fdb69
AlejandroSZIOSA <ggato33@gmail.com>
2025-02-28 16:33:19 +0100

    changed the function name

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const errorPage: React.FC = () => {
+const ErrorPage: React.FC = () => {
   return <div>ERROR page</div>;
 };
 
-export default errorPage;
+export default ErrorPage;
