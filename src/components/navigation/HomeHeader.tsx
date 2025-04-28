589f7148de3b767488db5afc0d9b1f5a2e60ff16
AlejandroSZIOSA <ggato33@gmail.com>
2025-04-28 08:50:53 +0200

    styling header and navigation

--- src/components/navigation/HomeHeader.tsx
+++ src/components/navigation/HomeHeader.tsx
@@ -2,6 +2,7 @@ import { type FC } from "react";
 import NavBar from "./NavBar";
 import { useUser_Ctx } from "../../store/user-Context";
 import { useNavigate } from "react-router-dom";
+import classes from "./HomeHeader.module.css";
 
 const HomeHeader: FC = () => {
   const { user_Token } = useUser_Ctx();
@@ -12,15 +13,17 @@ const HomeHeader: FC = () => {
   }
 
   return (
-    <header style={{ display: "flex", flexDirection: "row" }}>
+    <header className={classes.header}>
       {user_Token ? (
         <>
-          <img
-            src="/src/assets/icons/calculator.svg"
-            onClick={handleClick}
-            width={25}
-            height={25}
-          ></img>
+          <div style={{ marginLeft: "180px" }}>
+            <img
+              src="/src/assets/icons/calculator.svg"
+              onClick={handleClick}
+              width={25}
+              height={25}
+            ></img>
+          </div>
           <NavBar />
         </>
       ) : (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,21 @@
 import { type FC } from "react";
-import { Link } from "react-router-dom";
 import { useUser_Ctx } from "../store/user-Context";
 import HomeHeader from "../components/navigation/HomeHeader";
 import HomeFooter from "../components/HomeFooter";
+import classes from "./HomePage.module.css";
 
 const HomePage: FC = () => {
   const { user_Token } = useUser_Ctx();
-
-  /* console.log(user_data); */
   console.log(user_Token);
 
   return (
-    <>
+    <div>
       <HomeHeader />
       <main>
-        <h1>Home Page</h1>
-        <Link to="/calculator">
-          <button>Calculator</button>
-        </Link>
+        <h1>About Us</h1>
       </main>
       <HomeFooter />
-    </>
+    </div>
   );
 };
 
