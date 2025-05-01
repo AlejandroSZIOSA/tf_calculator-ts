948f8bec0df53240af08f44280e5ec82da36a72d
AlejandroSZIOSA <ggato33@gmail.com>
2025-05-01 11:18:17 +0200

    footer ready

--- src/components/HomeFooter.tsx
+++ src/components/HomeFooter.tsx
@@ -4,7 +4,29 @@ import classes from "./HomeFooter.module.css";
 const HomeFooter: FC = () => {
   return (
     <div className={classes.container}>
-      <h2>Footer</h2>
+      <div className={classes.innerContainer}>
+        <p>
+          <img
+            src="https://livewp.site/wp/md/agrosector/wp-content/uploads/sites/55/2019/01/icon-1.png"
+            alt="Phone"
+            data-lzl-src="https://livewp.site/wp/md/agrosector/wp-content/uploads/sites/55/2019/01/icon-1.png"
+            width={20}
+            height={20}
+          ></img>
+          +46 (08) 410 144 05
+        </p>
+
+        <p>
+          <img
+            src="https://livewp.site/wp/md/agrosector/wp-content/uploads/sites/55/2019/01/icon-2.png"
+            alt="E-Mail"
+            data-lzl-src="https://livewp.site/wp/md/agrosector/wp-content/uploads/sites/55/2019/01/icon-2.png"
+            width={20}
+            height={20}
+          />
+          info@turfquick.com
+        </p>
+      </div>
     </div>
   );
 };
